18667442b0973fa79d576f1ddd73a756e0b9ef7c
Александр Литвинов <alexpunder@yandex.ru>
2024-08-07 11:55:03 +0300

    В админ-панель добавлены оставшиеся модели, настроены взаимосвязи. Добавлена руссификация шаблонов.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,16 +1,20 @@
 from starlette_admin.contrib.sqla import Admin, ModelView
 from starlette_admin import (
     BooleanField, TimeField, StringField, IntegerField,
-    HasMany, HasOne, DateTimeField, DateField
+    HasMany, HasOne, DateTimeField, DateField, I18nConfig
 )
 
 from app.core.db import engine
 from app.models import (
     CarPost, Work, WorkOrder, Reservation,
-    User, Auto, Client, Master, MasterWork
+    Auto, Client, Master
 )
 
-admin = Admin(engine, title='Административная панель')
+admin = Admin(
+    engine,
+    title='Административная панель',
+    i18n_config=I18nConfig(default_locale="ru")
+)
 
 
 class CarPostView(ModelView):
@@ -40,47 +44,74 @@ class ReservationView(ModelView):
     ]
 
 
-# class WorkView(ModelView):
-#     identity = 'work'
-#     name = 'Работа'
-#     label = 'Список работ'
-#     fields = [
-#         StringField('title', label='Название работы'),
-#         IntegerField('price', label='Стоимость за единицу'),
-#         IntegerField('quantity', label='Количество'),
-#         HasOne('master', identity='master', label='Мастер'),
-#         HasOne('work_order', identity='workorder', label='Заказ-наряд'),
-#     ]
+class WorkOrderView(ModelView):
+    identity = 'workorder'
+    name = 'Заказ-наряд'
+    label = 'Список заказ-нарядов'
+    fields = [
+        DateTimeField('dt_to_create', label='Дата/время создания'),
+        StringField('description', 'Описание'),
+        HasMany('work', identity='work', label='Список работ'),
+        HasMany('reservation', identity='reservation', label='Резерв'),
+        HasOne('client', identity='client', label='Клиент')
+    ]
 
 
-# class WorkOrderView(ModelView):
-#     identity = 'workorder'
-#     name = 'Заказ-наряд'
-#     label = 'Список заказ-нарядов'
-#     fields = [
-#         DateTimeField('dt_to_create', label='Дата/время создания'),
-#         StringField('description', 'Описание'),
-#         HasMany('works', identity='workorder', label='Список работ')
-#     ]
+class WorkView(ModelView):
+    identity = 'work'
+    name = 'Работа'
+    label = 'Список работ'
+    fields = [
+        StringField('title', label='Название работы'),
+        IntegerField('price', label='Стоимость за единицу'),
+        IntegerField('quantity', label='Количество'),
+        HasOne('work_order', identity='workorder', label='Заказ-наряд'),
+        HasMany('masters', identity='master', label='Мастер')
+    ]
 
 
-# class MasterView(ModelView):
-#     identity = 'master'
-#     name = 'Мастер'
-#     label = 'Список мастеров'
-#     fields = [
-#         StringField('first_name', label='Имя'),
-#         StringField('last_name', label='Фамилия'),
-#         HasMany('works', identity='work', label='Работы')
-#     ]
+class MasterView(ModelView):
+    identity = 'master'
+    name = 'Мастер'
+    label = 'Список мастеров'
+    fields = [
+        StringField('first_name', label='Имя'),
+        StringField('last_name', label='Фамилия'),
+        HasMany('works', identity='work', label='Работы')
+    ]
 
 
-admin.add_view(ModelView(CarPost))
-admin.add_view(ModelView(Work))
-admin.add_view(ModelView(Master))
-admin.add_view(ModelView(WorkOrder))
-admin.add_view(ModelView(Reservation))
-admin.add_view(ModelView(User))
-admin.add_view(ModelView(Auto))
-admin.add_view(ModelView(MasterWork))
-admin.add_view(ModelView(Client))
+class AutoView(ModelView):
+    identity = 'auto'
+    name = 'Автомобили клиентов'
+    label = 'Автомобили'
+    fields = [
+        StringField('vin_code', label='VIN-код'),
+        StringField('mark', label='Марка'),
+        StringField('model', label='Модель'),
+        IntegerField('year', label='Год выпуска'),
+        IntegerField('mileage', label='Пробег'),
+        HasOne('client', identity='client', label='Клиент')
+    ]
+
+
+class ClientView(ModelView):
+    identity = 'client'
+    name = 'Клиенты'
+    label = 'Клиенты'
+    fields = [
+        StringField('first_name', label='Имя'),
+        StringField('last_name', label='Фамилия'),
+        StringField('phone_number', label='Номер телефона'),
+        HasMany('work_order', identity='workorder', label='Заказ-наряды'),
+        HasMany('auto', identity='auto', label='Автомобили')
+    ]
+
+
+admin.add_view(CarPostView(CarPost, identity='carpost'))
+admin.add_view(ReservationView(Reservation, identity='reservation'))
+admin.add_view(WorkOrderView(WorkOrder, identity='workorder'))
+admin.add_view(WorkView(Work, identity='work'))
+admin.add_view(AutoView(Auto, identity='auto'))
+admin.add_view(ClientView(Client, identity='client'))
+admin.add_view(MasterView(Master, identity='master'))
